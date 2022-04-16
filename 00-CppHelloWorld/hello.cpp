f72f5fe1d0d2714d4e3205844f96ba9840aa9b53
lubertoezequiel <lubertoezequiel@gmail.com>
2022-04-16 16:03:14 -0300

    Actualización de encabezado en hello.cpp

--- 00-CppHelloWorld/hello.cpp
+++ 00-CppHelloWorld/hello.cpp
@@ -1,5 +1,12 @@
+/*Ej 0: Hello World
+ *Realizar el hola mundo en C++
+ *Autor: Ezequiel Antonio Luberto
+ *Fecha: 16/4/2022
+*/
+
 #include <iostream>
 
-int main(){
- std::cout<<"Hello, World"; 
+int main()
+{
+    std::cout<<"Hello, World";
 }
